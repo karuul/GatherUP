9ee6e50d77960bcde1c3adf44e535bb5da5831cc
Aidas Balčaitis <balcaitis.aidas@gmail.com>
2017-05-14 15:38:36 +0300

    Configured to work with MySql, added identities.

--- GatherUp/GatherUp/Properties/AssemblyInfo.cs
+++ GatherUp/GatherUp/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a020590e-1cd8-49f5-a9af-786de8c8d2f2")]
+[assembly: Guid("d56ded9e-51b4-49ec-ab40-b3e097d8963b")]
 
 // Version information for an assembly consists of the following four values:
 //
