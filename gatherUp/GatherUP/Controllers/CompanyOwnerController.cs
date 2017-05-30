6a8e1c621731bf78380cc903b3160d5340701c90
Marius <marius.blazys16@gmail.com>
2017-05-30 05:30:43 +0300

    Place register and 85% place edit.

--- gatherUp/GatherUP/Controllers/CompanyOwnerController.cs
+++ gatherUp/GatherUP/Controllers/CompanyOwnerController.cs
@@ -23,8 +23,54 @@ namespace GatherUP.Controllers
 
         public ViewResult Index()
         {
-            var companyOwnerPlacesList = _context.Vietos.Where(c => c.Istaigos_savininkas.Prisijungimo_vardas == "savininkozmona").ToList();
+            //var companyOwnerPlacesList = _context.Vietos.Where(c => c.Istaigos_savininkas.Prisijungimo_vardas == "savininkozmona").ToList();
+            //return View(companyOwnerPlacesList);
+
+            var companyOwnerPlacesList = _context.Vietos.ToList();
             return View(companyOwnerPlacesList);
+
+        }
+
+        public ViewResult RegisterPlace(Vieta vieta)
+        {
+            if (vieta.Pavadinimas != null)
+            {
+                if (ModelState.IsValid)
+                {
+                    _context.Vietos.Add(vieta);
+                    _context.SaveChanges();
+                    var companyOwnerPlacesList = _context.Vietos.ToList();
+                    return View("Index", companyOwnerPlacesList);
+                }
+                else
+                {
+                    return View();
+                }
+            }
+            else
+            {
+                return View();
+            }
+            
+        }
+
+       public ActionResult Place(int id)
+        {
+            var companyOwnerPlacesList = _context.Vietos.Where(c => c.Id == id).ToList();
+            return View(companyOwnerPlacesList);
+        }
+
+        public ActionResult EditPlace(int id)
+        {
+            Vieta companyOwnerPlacesList = _context.Vietos.Find(id);
+            return View(companyOwnerPlacesList);
+        }
+        public ActionResult EditPlace(Vieta vieta)
+        {
+            
+
+            var companyOwnerPlacesList = _context.Vietos.ToList();
+            return View("Index", companyOwnerPlacesList);
         }
     }
 }
